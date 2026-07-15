8b3fd5105aa30059d3912625cff97706e692d09d
W1 CAPITAL <daviconcentrix@gmail.com>
2026-07-15 09:50:04 -0300

    Update auth-provider.tsx

--- src/components/auth/auth-provider.tsx
+++ src/components/auth/auth-provider.tsx
@@ -1,9 +1,3 @@
-/**
- * @fileOverview LexisPredict - W1 Capital Advanced Legal Operations
- * @copyright 2026 Davi Alves Figueredo / W1 Capital Assessoria Financeira Ltda.
- * @license Proprietary - All rights reserved.
- * @see LICENSE file for full terms.
- */
 
 "use client";
 
